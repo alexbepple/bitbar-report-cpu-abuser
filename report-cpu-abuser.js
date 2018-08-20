7966b723060d4418f46f6d6ed627dcf1c4bf0c6c
Alex Bepple <alex@bepple.de>
2018-08-20 07:17:17 +0200

    % Prettier

--- report-cpu-abuser.js
+++ report-cpu-abuser.js
@@ -1,39 +1,39 @@
 #!/usr/bin/env /usr/local/bin/node
 
-const r = require("ramda");
-const notifier = require("node-notifier");
-const si = require("systeminformation");
+const r = require('ramda')
+const notifier = require('node-notifier')
+const si = require('systeminformation')
 
 const findCulprit = r.pipe(
   r.filter(
     r.where({
-      state: r.equals("running"),
+      state: r.equals('running'),
       pcpu: r.gt(r.__, 30)
     })
   ),
-  r.sortBy(r.prop("pcpu")),
+  r.sortBy(r.prop('pcpu')),
   r.last
-);
+)
 
-const findCurrentCulprit = async () => findCulprit((await si.processes()).list);
+const findCurrentCulprit = async () => findCulprit((await si.processes()).list)
 
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
-(async () => {
-  const culprit1 = await findCurrentCulprit();
+;(async () => {
+  const culprit1 = await findCurrentCulprit()
   if (!culprit1) {
-    console.log("All good!");
-    return;
+    console.log('All good!')
+    return
   }
 
-  await sleep(500);
+  await sleep(500)
 
-  const culprit2 = await findCurrentCulprit();
-  if (culprit2 && r.eqBy(r.prop("command"), culprit1, culprit2)) {
-    console.log("Something's using lots of power.");
-    notifier.notify("Something's using lots of power. Better check.");
-    return;
+  const culprit2 = await findCurrentCulprit()
+  if (culprit2 && r.eqBy(r.prop('command'), culprit1, culprit2)) {
+    console.log("Something's using lots of power.")
+    notifier.notify("Something's using lots of power. Better check.")
+    return
   }
 
-  console.log("All good!");
-})();
+  console.log('All good!')
+})()
